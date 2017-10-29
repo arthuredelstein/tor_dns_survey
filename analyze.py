53531e2c634c237656d32e41174640e28357908d
Arthur Edelstein <arthuredelstein@gmail.com>
2017-10-29 19:58:24 -0400

    generating table

--- analyze.py
+++ analyze.py
@@ -1,4 +1,5 @@
 import json
+import urllib2
 
 def analyze_results(exit_results, address):
     exit_results_example = exit_results[address]
@@ -16,3 +17,27 @@ def analyze_results(exit_results, address):
     flaky = len(filter(lambda x: x < 1.0 and x > 0.0, rates_alone))
     best = len(filter(lambda x: x == 0.0, rates_alone))
     return timeout_rates, average, worst, flaky, best
+
+def relay_data(fingerprint):
+    url = "https://onionoo.torproject.org/details?limit=1&search=" + fingerprint
+    response = urllib2.urlopen(url)
+    data = json.load(response)
+    return data["relays"][0]
+
+def build_table(timeout_rates):
+    headers = None
+    s = u""
+    for fingerprint in timeout_rates.keys():
+        relay = relay_data(fingerprint)
+        relay["timeout_rate_21394"] = timeout_rates[fingerprint]
+        if headers == None:
+            headers = map(str, sorted(relay.keys()))
+            headers.remove("fingerprint")
+            headers.remove("timeout_rate_21394")
+            headers.insert(0, "fingerprint")
+            headers.insert(1, "timeout_rate_21394")
+            print headers
+            s += "\t".join(headers) + "\n"
+        s += "\t".join([unicode(relay.get(header, "")) for header in headers]) + "\n"
+    with open("relay_timeout_table.txt", "w") as f:
+        f.write(s.encode('ascii', 'ignore')
