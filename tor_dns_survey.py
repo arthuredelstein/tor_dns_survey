d45fd304f0a27d8f2d8386bbe8ed602c5ab66912
Arthur Edelstein <arthuredelstein@gmail.com>
2018-09-12 19:57:18 -0400

    fix handling of errors

--- tor_dns_survey.py
+++ tor_dns_survey.py
@@ -43,7 +43,7 @@ def test_exit(fingerprint, address):
     except:
         print "circuit build failed", sys.exc_info()
         delta = time.time() - start
-        record_exit_result(fingerprint, address, sys.exc_info()[1], delta)
+        record_exit_result(fingerprint, address, str(sys.exc_info()[1]), delta)
         return
     def attach_stream(stream):
         delta = time.time() - start
@@ -60,7 +60,7 @@ def test_exit(fingerprint, address):
         test_http_request(address)
     except:
         print "error: ", sys.exc_info()
-        record_exit_result(fingerprint, address, sys.exc_info()[1], time.time() - start)
+        record_exit_result(fingerprint, address, str(sys.exc_info()[1]), time.time() - start)
     finally:
         controller.remove_event_listener(attach_stream)
         controller.reset_conf('__LeaveStreamsUnattached')
@@ -77,6 +77,7 @@ def test_exits(exits, addresses, repeats):
 
 def write_json(filestem, data):
     now = datetime.datetime.now().strftime("%Y%m%d_%H%M");
+    print(data)
     jsonStr = json.dumps(data)
     with open(filestem + "_" + now + ".json", "w") as f:
         f.write(jsonStr)
@@ -100,21 +101,23 @@ def test_all_exits(addresses = ["example.com", "93.184.216.34"], repeats = 10):
 
 #ExcludeExitNodes node,node,...
 special_exits = [
-    "204DFD2A2C6A0DC1FA0EACB495218E0B661704FD", # HaveHeart
-#    "77131D7E2EC1CA9B8D737502256DA9103599CE51", # CriticalMass
-#    "1D3174338A1131A53E098443E76E1103CDED00DC", # criticalmass
+#    "204DFD2A2C6A0DC1FA0EACB495218E0B661704FD", # HaveHeart
+    "77131D7E2EC1CA9B8D737502256DA9103599CE51", # CriticalMass
+    "1D3174338A1131A53E098443E76E1103CDED00DC", # criticalmass
 #    "7BFB908A3AA5B491DA4CA72CCBEE0E1F2A939B55", # sofia
 #    "09FA8B4F665AD65D2C2A49870F1AA3BA8811E449", # StanMarsh
 #    "335746A6DEB684FABDF3FC5835C3898F05C5A5A8", # KyleBroflovksi
 #    "B0279A521375F3CB2AE210BDBFC645FDD2E1973A", # chulak
-    "0593F5255316748247EBA76353A3A61F62224903", # novatorrelay
+#    "0593F5255316748247EBA76353A3A61F62224903", # novatorrelay
 #    "696ABFA60C2FEA676FAF2DC2DA58A6D09FDBF78C", # HorstHanfblatt
 #    "A9EBCBCB0EC01FEE8480C02214E4120B1C17ACF7", # labaudric
 #    "D9F004C4664E9EE5AED955F91A67A5405531F33C"  # SophieScholl
-    "366E36894AF7ED5AEAE3D26FBEBD3FA29AA34FDD" #SentriesExit2
+#    "366E36894AF7ED5AEAE3D26FBEBD3FA29AA34FDD" #SentriesExit2
 ]
 
 def test_special_exits():
-    test_exits(special_exits, ["example.com", "93.184.216.34"], 5)
+    test_exits(special_exits, ["example.com", "93.184.216.34"], 1)
+    print(exit_results)
+    write_json("../all_exit_results/exit_results", exit_results);
 
 
